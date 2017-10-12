281f1ec033353bf34f2cc78cd8e7cb7b790058f1
Douglas juliao <douglas_juliao16@hotmail.com>
2017-10-12 18:46:45 -0300

    Created html with json and replaced images url

--- challenge/js/Carousel.js
+++ challenge/js/Carousel.js
@@ -1,3 +1,37 @@
 class Carousel{
-    constructor(config){ }
+    constructor(config){ 
+        this.quantity = config.quantity; // Quantity of itens to show
+        this.data = config.data;
+        this.selector = config.selector;
+    }
+    createHTML(){
+        let html = '';
+        let dt = this.data;
+
+        for(let i = 0, len = dt.length; i < len; i++){
+            let productPayment = dt[i].productInfo.paymentConditions.match(/R\$.+[0-9]/); //Only Price
+            let productTimes = dt[i].productInfo.paymentConditions.match(/\w.+x/); //Only Times
+
+            html+= `<div class="item grid">
+                    <img class="responsive-img" src="${dt[i].imageName}" alt="Image desc">
+                    <div class="description">
+                        ${dt[i].name}
+                    </div>
+                    <div class="price">
+                        <p>Por:<strong>${dt[i].price}</strong></p>
+                        <p><strong>${productTimes}</strong> de <strong>${productPayment}</strong> sem juros</p>
+                    </div>
+                    <div class="btn add-cart">Adicionar ao carrinho<i class="material-icons">add_shopping_cart</i></div>
+                </div>`;
+        }
+        html+= `<div class="nav-carousel">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>`;
+        return html;
+    }
+    init(){
+        this.selector.innerHTML = this.createHTML();
+    }
 }
--- challenge/js/main.js
+++ challenge/js/main.js
@@ -1,5 +1,21 @@
-var json = new HttpRequest('GET','../json/products.json');
+let jsonRequest = new HttpRequest('GET','../json/products.json');
 
-json.request(function(json){
-    console.log(json);
+jsonRequest.request(function(data){
+    if(!data){
+        return false;
+    }
+
+    //CAROUSEL CONF
+    const config = {
+        selector: document.querySelector('#carousel_shelf'),
+        quantity: 4,
+        data: data[0].data.recommendation
+    };
+
+    let carousel = new Carousel(config);
+    carousel.init();
+
+
+    console.log('DATA: ',data);
+    console.log('SELECTOR: ',carousel);
 })
